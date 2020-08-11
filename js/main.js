7e1d9561e848eb3dba38d0903c4e50168808fc5d
KARIMOV MOHIR <60309016+karimov4181@users.noreply.github.com>
2020-08-11 18:59:35 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function (){
+    
+    $(" .slider_inner, .news__slider-inner").slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite:false
+    });
+    
+    $('select').styler(); 
+    
+    
+    $('.header_btn-menu').on('click', function () {
+        $('.menu ul').slideToggle();
+    }) ;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}); 
